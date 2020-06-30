43348606c29e334c2f696145d3fe65be6ec0ed84
ciazhar <ciazhar.id@gmail.com>
2020-06-30 10:28:00 +0700

    init config

---
+++ gen/template/init/config.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"github.com/ciazhar/generate/gen/template/data"
+	"os"
+	"strings"
+	"text/template"
+)
+
+func main() {
+	funcMap := template.FuncMap{
+		"toLower": strings.ToLower,
+	}
+
+	var d data.Data
+	flag.StringVar(&d.Package, "package", "github.com/ciazhar/example", "The package used for the queue being generated")
+	flag.Parse()
+
+	t := template.Must(template.New("queue").Funcs(funcMap).Parse(ConfigTemplate))
+	t.Execute(os.Stdout, d)
+}
+
+var ConfigTemplate = `
+{
+  "name": "GRPC COMMON CONFIG",
+  "profile": "debug",
+  "version": "v6.1.1",
+  "port":"8080",
+  "postgres.host": "localhost",
+  "postgres.username": "postgres",
+  "postgres.password": "",
+  "postgres.database": "orm_test",
+  "postgres.port": "5432",
+  "grpc.address": "0.0.0.0:50051"
+}
+`
